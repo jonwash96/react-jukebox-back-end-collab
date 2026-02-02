4fe3c671d126cd80d5c45eb2714b487be2935533
JON Washington <jonwashPro@gmail.com>
2026-02-02 16:09:18 -0600

    Setup Server & Package.json

---
+++ README.md
@@ -0,0 +1,5 @@
+# React Jukebox Backend Collab
+
+![Reactville Image](https://pages.git.generalassemb.ly/modular-curriculum-all-courses/express-api-jukebox-back-end-lab/assets/reactville.png)
+
+Reactville’s community center is launching a public jukebox, and they need your expertise to build the backend API that powers it! This jukebox will allow residents to freely add, update, and delete music tracks from a shared community playlist.
---
+++ server.js
@@ -0,0 +1,30 @@
+//* MNT
+require('dotenv').config();
+require('./db/connection.js');
+const express = require('express');
+const morgan = require('morgan');
+const cors = require('cors');
+
+//* DATA
+
+//* VAR
+const PORT = process.env.PORT || 3000;
+
+//* APP
+const app = express();
+
+//* MID
+app.use(express.json());
+app.use(morgan('tiny'));
+app.use(cors)
+
+//* ROUTE
+app.get('/', (req,res) => {
+    res.status(201).json({
+        message:"This is the home route"
+    })
+})
+
+
+//* IO
+app.listen(PORT, ()=>console.log(`Server Running on port ${PORT}. Access at [http://localhost:${PORT}]`))
